23457f1c52f31491136069fd092b9eb69e58e83d
renan-cavalcante <renancggama@gmail.com>
2023-09-11 21:49:08 -0300

    exercicio5

---
+++ Threads/exercicio5/src/exercicio5/controller/PingController.java
@@ -0,0 +1,58 @@
+package exercicio5.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PingController extends Thread {
+
+	public PingController() {
+	}
+	
+	@Override
+	public void run() {
+		ping("google");
+		ping("uol");
+		ping("terra");
+	}
+
+	
+	private void ping(String servidor) {
+		
+		
+		
+		try (BufferedReader buffer = new BufferedReader(new InputStreamReader(callProcess("ping -4 -c 10 www."+servidor+".com.br ").getInputStream()))){
+						
+			String linha;
+			linha = buffer.readLine();
+			
+			
+			while(linha != null) {
+				System.out.println(servidor+"");
+				if(linha.toLowerCase().contains("rtt")) { 
+					
+					System.out.println( linha.split("/")[4] + "ms");
+				}else if(linha.toLowerCase().contains("reply"))  {
+					System.out.println((linha.split(" ")[4]).split("=")[1]);
+				}
+				linha = buffer.readLine();
+			}
+			buffer.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private Process callProcess(String process) {
+		try {
+			return Runtime.getRuntime().exec(process);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		return null;	
+	}
+
+}
---
+++ Threads/exercicio5/src/exercicio5/view/Program.java
@@ -0,0 +1,14 @@
+package exercicio5.view;
+
+import exercicio5.controller.PingController;
+
+public class Program {
+
+	public static void main(String[] args) {
+		PingController ping = new PingController();
+		
+		ping.start();
+
+	}
+
+}
